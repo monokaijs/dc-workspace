39d8a9cdfe0ee89542067246e05322f88e1be1fb
monokaijs <monokaijs@gmail.com>
2025-09-06 12:17:21 +0700

    enable notarization

--- package-mac-signed.sh
+++ package-mac-signed.sh
@@ -1,5 +1,5 @@
 export APPLE_ID="monokaijs@gmail.com"
-export APPLE_APP_SPECIFIC_APP_PASSWORD="nmvs-blyg-rart-equy"
+export APPLE_APP_SPECIFIC_PASSWORD="nmvs-blyg-rart-equy"
 export APPLE_TEAM_ID=""
 export CSC_LINK="./certificate.p12"
 export CSC_KEY_PASSWORD="M@sterPwd1"
