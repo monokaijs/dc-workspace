a58184944767697b47f5ac8f763eb058d4dd507d
monokaijs <monokaijs@gmail.com>
2025-09-06 11:52:21 +0700

    fix mac builds

---
+++ package-mac-signed.sh
@@ -0,0 +1,6 @@
+export APPLE_ID="monokaijs@gmail.com"
+export APPLE_APP_SPECIFIC_APP_PASSWORD="nmvs-blyg-rart-equy"
+export APPLE_TEAM_ID=""
+export CSC_LINK="./certificate.p12"
+export CSC_KEY_PASSWORD="M@sterPwd1"
+yarn build:mac
